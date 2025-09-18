53a6672c39d86aa3fe88c3ecb7c7b70465f1e5fd
justsicklife <justsicklife@gmail.com>
2025-09-18 16:04:18 +0900

    test commit

---
+++ src/Foo.java
@@ -0,0 +1,12 @@
+public class Foo {
+    public static void main(String[] args) {
+        var price = 200;
+        var name = "Foo";
+        var pi = 3.14;
+        System.out.println("price = " + (price+1000));
+        System.out.println("name = " + name);
+        System.out.println("pi = " + (pi + 100));
+        System.out.println("Foo");
+        System.out.printf("price = %d\n",price + 100);
+    }   
+}
